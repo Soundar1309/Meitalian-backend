f302246913cc285818142d62689982c8d28f0f20
Aravinth <ambionaravinth@gmail.com>
2024-05-31 18:23:52 +0530

    fixes

--- api/routes/orderRoutes.js
+++ api/routes/orderRoutes.js
@@ -1,16 +1,10 @@
 const express = require("express");
-// const Carts = require("../models/Carts");
-// const Order = require("../models/Order");
 const router = express.Router();
 
 const orderController = require("../controllers/orderControllers");
 
 const verifyToken = require("../middlewares/verifyToken");
 const verifyAdmin = require("../middlewares/verifyAdmin");
-
-// router.get("/", verifyToken, (req, res) => {
-//   orderController.getOrderByEmail(req, res);
-// });
 
 router.get("/", orderController.getOrderByEmail);
 
